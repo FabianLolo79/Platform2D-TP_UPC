7a224a061faa03a8465d23617dd8018dbebfb091
Elaias98 <eliasd0203@gmail.com>
2024-11-27 20:16:00 -0300

    animaciones en food

--- Assets/Scripts/Collected/Fruit/FruitCollected.cs
+++ Assets/Scripts/Collected/Fruit/FruitCollected.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class FruitCollected : MonoBehaviour
@@ -5,15 +6,24 @@ public class FruitCollected : MonoBehaviour
     [SerializeField] private Animator _animator;
     [SerializeField] private int _points;
 
+    private bool _waitingToDisable = false;
+
+    private IEnumerator DisableDelay()
+    {
+        yield return new WaitForSeconds(1);
+        gameObject.SetActive(false);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            var player = collision.gameObject.GetComponent<PlayerRoo>();
-            player.AddScore(_points);
+        if (!collision.CompareTag("Player") || _waitingToDisable) return;
+        
+        var player = collision.gameObject.GetComponent<PlayerRoo>();
+        player.AddScore(_points);
 
-            //_animator.SetBool("Hit", true);
-            gameObject.SetActive(false);
-        }
+        _animator.SetBool("Hit", true);
+        StartCoroutine(DisableDelay());
+            
+        _waitingToDisable = true;   
     }
 }
